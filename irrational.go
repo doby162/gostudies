1c7f209f2c7dca4b95693bde7ffc5a890775e540
Michael Dorian <doby162@gmail.com>
2020-12-20 17:19:01 -0600

    refactor to allow early exit when answer is found. more verbos output

--- irrational.go
+++ irrational.go
@@ -7,19 +7,37 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
+	maxIters := 25 // remove this check if you get your hands on a super computer
 	rootOf, _ := strconv.ParseFloat(os.Args[1], 64)
-	answer := newt(rootOf, 15)
+	guess := new(big.Float).SetInt64(1)
+
+	for i := 0; i <= maxIters; i++ {
+		var t = time.Now()
+		answer := newt(rootOf, float64(i), guess)
+		bigstr := guessToText(answer)
+		index := strings.Index(bigstr, os.Args[2])
+		fmt.Println("took", time.Now().Sub(t))
+		if index != -1 {
+			fmt.Println("\nFound \"", os.Args[2], "\" at index ", index, "in the base24 aproximation of sqrt(", os.Args[1], ") after ", i, " iterations")
+			i = maxIters + 1
+		}
+	}
+	fmt.Println("finished")
+}
+
+func guessToText(answer *big.Float) string {
 	file, _ := os.Create("./sqrt.txt")
 	defer file.Close()
 	bigstr := fmt.Sprint(answer)
 	bigstr = strings.Replace(bigstr, ".", "", 1)
 	var bignum, _ = new(big.Int).SetString(bigstr, 0)
 	bigstr = TextEncode(bignum.Text(26))
-	file.WriteString(bigstr)
-	fmt.Println(strings.Index(bigstr, os.Args[2]))
+	// file.WriteString(bigstr)
+	return bigstr
 }
 
 func TextEncode(x string) string {
@@ -52,7 +70,7 @@ func TextEncode(x string) string {
 	return x
 }
 
-func newt(rootOf float64, steps float64) *big.Float {
+func newt(rootOf float64, step float64, guess *big.Float) *big.Float {
 	// Since Newton's Method doubles the number of correct digits at each
 	// iteration, we need at least log_2(prec) steps.
 	// steps := int(math.Log2(prec))
@@ -61,7 +79,7 @@ func newt(rootOf float64, steps float64) *big.Float {
 	// log2(prec)=steps == prec=2^steps
 	// finally, we actually want digits rather than bits,
 	// so we multiply the bits by log2(10)
-	var prec = uint(math.Log2(10) * math.Exp2(steps))
+	var prec = uint(math.Log2(10) * math.Exp2(step))
 	// not all digits are going to be correct, but that's ok
 
 	// Compute the square root of 2 using Newton's Method. We start with
@@ -73,7 +91,7 @@ func newt(rootOf float64, steps float64) *big.Float {
 	half := new(big.Float).SetPrec(prec).SetFloat64(0.5)
 
 	// Use 1 as the initial estimate.
-	guess := new(big.Float).SetPrec(prec).SetInt64(1)
+	guess.SetPrec(prec)
 
 	// We use t as a temporary variable. There's no need to set its precision
 	// since big.Float values with unset (== 0) precision automatically assume
@@ -82,11 +100,9 @@ func newt(rootOf float64, steps float64) *big.Float {
 	t := new(big.Float)
 
 	// Iterate.
-	for i := 0.0; i <= steps; i++ {
-		fmt.Println("calculating log2(10) * exp2(", i, ") (", int(math.Log2(10)*math.Exp2(i)), ") bits...")
-		t.Quo(ro, guess)   // t = 2.0 / guess_n
-		t.Add(guess, t)    // t = guess_n + (2.0 / guess_n)
-		guess.Mul(half, t) // guess_{n+1} = 0.5 * t
-	}
+	fmt.Println("calculating log2(10) * exp2(", step, ") (", prec, ") bits...")
+	t.Quo(ro, guess)   // t = 2.0 / guess_n
+	t.Add(guess, t)    // t = guess_n + (2.0 / guess_n)
+	guess.Mul(half, t) // guess_{n+1} = 0.5 * t
 	return guess
 }
